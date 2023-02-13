d84dd5c29121ccf03e0326f41e06b74e453b5e50
andrijs29 <122866313+andrijs29@users.noreply.github.com>
2023-02-14 04:21:39 +0700

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,30 @@
+cd /usr/local/CyberCP/public
+mkdir phpmyadmin
+mv doc/ phpmyadmin/
+mv examples/ phpmyadmin/
+mv js/ phpmyadmin/
+mv libraries/ phpmyadmin/
+mv locale/ phpmyadmin/
+mv setup/ phpmyadmin/
+mv sql/ phpmyadmin/
+mv templates/ phpmyadmin/
+mv themes/ phpmyadmin/
+mv vendor/ phpmyadmin/
+mv babel.config.json phpmyadmin/
+mv ChangeLog phpmyadmin/
+mv composer.json phpmyadmin/
+mv composer.lock phpmyadmin/
+mv config.sample.inc.php phpmyadmin/
+mv CONTRIBUTING.md phpmyadmin/
+mv favicon.ico phpmyadmin/
+mv index.php phpmyadmin/
+mv LICENSE phpmyadmin/
+mv package.json phpmyadmin/
+mv print.css phpmyadmin/
+mv README phpmyadmin/
+mv RELEASE-DATE-5.1.3 phpmyadmin/
+mv robots.txt phpmyadmin/
+mv show_config_errors.php phpmyadmin/
+mv url.php phpmyadmin/
+mv yarn.lock phpmyadmin/
+echo "Fixed"
