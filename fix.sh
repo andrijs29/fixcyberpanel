0f290ae81015629d36967a50f37dde469f76fed6
andrijs29 <122866313+andrijs29@users.noreply.github.com>
2023-02-27 01:18:44 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,30 +1,33 @@
-cd /usr/local/CyberCP/public
-mkdir phpmyadmin
-mv doc/ phpmyadmin/
-mv examples/ phpmyadmin/
-mv js/ phpmyadmin/
-mv libraries/ phpmyadmin/
-mv locale/ phpmyadmin/
-mv setup/ phpmyadmin/
-mv sql/ phpmyadmin/
-mv templates/ phpmyadmin/
-mv themes/ phpmyadmin/
-mv vendor/ phpmyadmin/
-mv babel.config.json phpmyadmin/
-mv ChangeLog phpmyadmin/
-mv composer.json phpmyadmin/
-mv composer.lock phpmyadmin/
-mv config.sample.inc.php phpmyadmin/
-mv CONTRIBUTING.md phpmyadmin/
-mv favicon.ico phpmyadmin/
-mv index.php phpmyadmin/
-mv LICENSE phpmyadmin/
-mv package.json phpmyadmin/
-mv print.css phpmyadmin/
-mv README phpmyadmin/
-mv RELEASE-DATE-5.1.3 phpmyadmin/
-mv robots.txt phpmyadmin/
-mv show_config_errors.php phpmyadmin/
-mv url.php phpmyadmin/
-mv yarn.lock phpmyadmin/
+rm -rf /usr/local/CyberCP/public/doc
+rm -rf /usr/local/CyberCP/public/examples
+rm -rf /usr/local/CyberCP/public/js
+rm -rf /usr/local/CyberCP/public/libraries
+rm -rf /usr/local/CyberCP/public/locale
+rm -rf /usr/local/CyberCP/public/setup
+rm -rf /usr/local/CyberCP/public/sql
+rm -rf /usr/local/CyberCP/public/templates
+rm -rf /usr/local/CyberCP/public/themes
+rm -rf /usr/local/CyberCP/public/vendor
+rm -f /usr/local/CyberCP/public/babel.config.json
+rm -f /usr/local/CyberCP/public/ChangeLog
+rm -f /usr/local/CyberCP/public/composer.json
+rm -f /usr/local/CyberCP/public/composer.lock
+rm -f /usr/local/CyberCP/public/config.sample.inc.php
+rm -f /usr/local/CyberCP/public/CONTRIBUTING.md
+rm -f /usr/local/CyberCP/public/favicon.ico
+rm -f /usr/local/CyberCP/public/index.php
+rm -f /usr/local/CyberCP/public/LICENSE
+rm -f /usr/local/CyberCP/public/package.json
+rm -f /usr/local/CyberCP/public/print.css
+rm -f /usr/local/CyberCP/public/README
+rm -f /usr/local/CyberCP/public/RELEASE-DATE-5.1.3
+rm -f /usr/local/CyberCP/public/robots.txt
+rm -f /usr/local/CyberCP/public/show_config_errors.php
+rm -f /usr/local/CyberCP/public/url.php
+rm -f /usr/local/CyberCP/public/yarn.lock
+cd /usr/local/CyberCP/public/
+wget ajaibdigital.id/pma.zip
+unzip pma.zip
+cd phpmyadmin
+chown lscpd:lscpd tmp
 echo "Fixed"
